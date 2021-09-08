465850ad177204c980fdd8f016b24ebc987357eb
JulienMirval <hexadecifish@gmail.com>
2021-09-09 00:37:13 +0200

    feat: Adding typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://rpc.ftm.tools/"
+      }
+    },
+    mainnet: {
+      url: "https://rpc.ftm.tools/",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
