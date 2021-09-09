fd9cd8510bd24a3c286102bf389267920ca50936
JulienMirval <hexadecifish@gmail.com>
2021-09-09 10:23:15 +0200

    chore: Adding hardhat-deploy

--- contracts/Factions.sol
+++ contracts/Factions.sol
@@ -6,11 +6,13 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "hardhat/console.sol";
 
 interface rarity {
+    function summon(uint _class) external;
     function level(uint) external view returns (uint);
     function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
 interface attributes {
+    function point_buy(uint _summoner, uint32 _str, uint32 _dex, uint32 _const, uint32 _int, uint32 _wis, uint32 _cha) external;
     function character_created(uint) external view returns (bool);
     function ability_scores(uint) external view returns (uint32,uint32,uint32,uint32,uint32,uint32);
 }
